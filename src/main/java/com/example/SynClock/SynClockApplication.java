d8f4f1df58b5fe200f183bd1320789a018adadcc
EnesTopal <enes.topal1@gazi.edu.tr>
2025-01-15 21:00:16 +0300

    project created

---
+++ src/main/java/com/example/SynClock/SynClockApplication.java
@@ -0,0 +1,14 @@
+package com.example.SynClock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynClockApplication.class, args);
+		System.out.println("Runs");
+	}
+
+}
---
+++ src/test/java/com/example/SynClock/SynClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SynClock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
